e75566a3cee8a07350adc80b727e882d98e64a36
factscout <spacejumper05@gmail.com>
2024-01-26 11:01:45 +0100

    hans get ze flamenwefer

---
+++ M320_HotelmanagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace M320_HotelmanagementSystem.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string html = System.IO.File.ReadAllText("web/login.html");
+            return Content(html, "text/html");
+        }
+
+       
+    }
+}
